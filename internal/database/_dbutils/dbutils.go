568ccab918424aa75f88ac2f947f550ad1ae015a
ramcd86 <ra_mcd@yahoo.com>
2024-03-11 21:09:01 +0000

    Adding inserts.

--- internal/database/_dbutils/dbutils.go
+++ internal/database/_dbutils/dbutils.go
@@ -5,10 +5,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"sync"
 
 	"fandm/environment"
+	"fandm/internal/utls"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -24,17 +25,9 @@ func GetDbConnectionString() string {
 
 func TestConnection() bool {
 	db, err := sql.Open("mysql", GetDbConnectionString())
-	if err != nil {
-		fmt.Println("Failed to connect to Mysql Database with the following error: ", err)
-		return false
-	}
-
+	utls.Catch(err)
 	err = db.Ping()
-	if err != nil {
-		fmt.Println("Failed to ping the Mysql Database with the following error: ", err)
-		return false
-	}
-
+	utls.Catch(err)
 	fmt.Println("Successfully connected to the Mysql Database")
 
 	return true
@@ -43,94 +36,107 @@ func TestConnection() bool {
 func SetUpDatabase() {
 	if TestConnection() {
 		db, err := sql.Open("mysql", GetDbConnectionString())
-		if err != nil {
-			fmt.Println("Failed to connect to Mysql Database with the following error: ", err)
-			return
-		}
+		utls.Catch(err)
 		// Create primary interactive tables.
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS treatments 
+			( id INT AUTO_INCREMENT PRIMARY KEY, 
+			treatment_name TEXT NOT NULL, 
+			treatment_description TEXT NOT NULL, 
+			actor_interactions TEXT NOT NULL, 
+			condition_interactions TEXT NOT NULL)`)
+		utls.Catch(err)
 
-		_, err = db.Exec("CREATE TABLE IF NOT EXISTS treatments ( id INT AUTO_INCREMENT PRIMARY KEY, treatment_name TEXT NOT NULL, treatment_description TEXT NOT NULL, actort_interactions TEXT NOT NULL, condition_interactions TEXT NOT NULL);")
-		if err != nil {
-			fmt.Println("Failed to create Schema 'treatments'", err)
-			return
-		}
-		_, err = db.Exec("CREATE TABLE IF NOT EXISTS actors ( id INT AUTO_INCREMENT PRIMARY KEY, actor_name TEXT NOT NULL, actor_description TEXT NOT NULL, treatment_interactions TEXT NOT NULL, condition_interactions TEXT NOT NULL);")
-		if err != nil {
-			fmt.Println("Failed to create Schema 'actors'", err)
-			return
-		}
-		_, err = db.Exec("CREATE TABLE IF NOT EXISTS conditions ( id INT AUTO_INCREMENT PRIMARY KEY, condition_name TEXT NOT NULL, condition_description TEXT NOT NULL, actort_interactions TEXT NOT NULL, treatment_interactions TEXT NOT NULL);")
-		if err != nil {
-			fmt.Println("Failed to create Schema 'conditions'", err)
-			return
-		}
-		_, err = db.Exec("CREATE TABLE IF NOT EXISTS reports ( id INT AUTO_INCREMENT PRIMARY KEY, reporter_condition VARCHAR(255) NOT NULL, reporter_treatment VARCHAR(255) NOT NULL, reporter_actor VARCHAR(255) NOT NULL);")
-		if err != nil {
-			fmt.Println("Failed to create Schema 'reports'", err)
-			return
-		}
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS actors 
+			( id INT AUTO_INCREMENT PRIMARY KEY, 
+			actor_name TEXT NOT NULL, 
+			actor_description TEXT NOT NULL, 
+			treatment_interactions TEXT NOT NULL, 
+			condition_interactions TEXT NOT NULL);`)
+		utls.Catch(err)
+
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS conditions 
+			( id INT AUTO_INCREMENT PRIMARY KEY, 
+			condition_name TEXT NOT NULL, 
+			condition_description TEXT NOT NULL, 
+			actor_interactions TEXT NOT NULL, 
+			treatment_interactions TEXT NOT NULL);`)
+		utls.Catch(err)
+
+		_, err = db.Exec(`CREATE TABLE IF NOT EXISTS reports 
+			( id INT AUTO_INCREMENT PRIMARY KEY, 
+			reporter_condition VARCHAR(255) NOT NULL, 
+			reporter_treatment VARCHAR(255) NOT NULL, 
+			reporter_actor VARCHAR(255) NOT NULL);`)
+		utls.Catch(err)
 
 		createAndPopulateTables("foods")
 		createAndPopulateTables("diseases")
-		creareAndPopulateTables("treatments")
+		createAndPopulateTables("treatments")
 	}
 }
 
 func createAndPopulateTables(insertType string) {
-	var filePath string
+	wg := sync.WaitGroup{}
+
+	grabData := func(filePath string, insertQuery string) {
+		defer wg.Done()
+
+		var result map[string]string
+
+		jsonFile, err := os.Open(filePath)
+		utls.Catch(err)
+
+		defer jsonFile.Close()
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+
+		json.Unmarshal([]byte(byteValue), &result)
+
+		insertActors(result, insertQuery)
+	}
 
 	switch insertType {
 	case "foods":
-		filePath = "internal/_json/actors/foods.json"
+		wg.Add(1)
+		go grabData("internal/_json/actors/foods.json",
+			`
+            INSERT INTO actors
+                (actor_name, actor_description, treatment_interactions, condition_interactions)
+            VALUES(?, ?, ?, ?)
+        `)
 	case "diseases":
-		filePath = "internal/_json/conditions/diseases.json"
+		wg.Add(1)
+		go grabData("internal/_json/conditions/diseases.json",
+			`
+            INSERT INTO conditions
+                (condition_name, condition_description, actor_interactions, treatment_interactions)
+            VALUES(?, ?, ?, ?)
+        `)
 	case "treatments":
-		filePath = "internal/_json/treatments/treatments.json"
+		wg.Add(1)
+		go grabData("internal/_json/treatments/treatments.json",
+			`
+            INSERT INTO 
+                treatments(treatment_name, treatment_description, actor_interactions, condition_interactions)
+            VALUES(?, ?, ?, ?)
+        `)
 	}
 
-	jsonFile, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer jsonFile.Close()
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var result map[string]string
-
-	json.Unmarshal([]byte(byteValue), &result)
-
-	insertActors(result)
+	wg.Wait()
 }
 
-func insertActors(actorsMap map[string]string) {
+func insertActors(dataMap map[string]string, insertQuery string) {
 	db, err := sql.Open("mysql", GetDbConnectionString())
-	if err != nil {
-		log.Fatal(err)
-	}
+	utls.Catch(err)
+
 	defer db.Close()
 
-	stmt, err := db.Prepare("INSERT INTO actors(actor_name, actor_description, treatment_interactions, condition_interactions) VALUES(?, ?, ?, ?)")
-	if err != nil {
-		log.Fatal(err)
-	}
+	stmt, err := db.Prepare(insertQuery)
+	utls.Catch(err)
 
-	for key, value := range actorsMap {
+	for key, value := range dataMap {
 		_, err = stmt.Exec(key, value, "", "")
-		if err != nil {
-			log.Fatal(err)
-		}
+		utls.Catch(err)
 	}
 
 	fmt.Println("Data inserted successfully.")
-}
-
-func createAndPopulateConditionsTable() {
-}
-
-func createAndPopulateTreatmentsTable() {
 }
